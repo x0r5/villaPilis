c99ffbc3df6d186f666c1c207b1af38b4c1be7e9
x0r5 <obivan05@gmail.com>
2022-02-21 09:03:56 +0100

    moving to php base

---
+++ index.php
@@ -0,0 +1,133 @@
+  <?php include('header.php'); ?>
+
+
+  <div class="row image-row">
+    <div id="image-big-2" class="col-12 image-big relax" data-rellax-speed="2">
+    </div>
+    <div class="over-text over-text-2">Mert kell egy hely</div>
+  </div>
+  </div>
+
+
+  <div class="container-fluid container-2">
+    <div class="row">
+      <div class="title-main">Miért gyertek hozzánk?</div>
+    </div>
+
+    <div class="row row-2">
+      <div class="col-md">
+        <div class="title-2" onclick="location.href = '/wellness'" >Wellness</div><a href="/wellness">Tovább ></a>
+        <div class="text-2"><p>
+          A napozás és fürdés kedvelőinek a kertben lévő 70m2-es Wellness teraszt ajánljuk. Rajta egy hatszor háromméteres egyméterhúsz centi mély fűtött úszómedence – mely április 15 és október 1-e között üzemel -található.
+        </p>
+        <p>
+          A teraszon napozó ágyak és „napozó zsákok” biztosítják a gondtalan pihenést. A 6 személyes finn szauna mellette csobbanó medence található. A szauna nagy méretű ablaka a kertre néz, ami télen csodálatos kontrasztot teremt a gyönyörű havas táj látványa és a szauna melege között. 
+         </p></div>
+      </div>
+      <div class="col-md-8">
+        <div data-aos="fade-left" data-aos-duration="1000" data-aos-offset="200">
+          <img class="img-fluid" src="img/medence-1.jpg">
+        </div>
+      </div>
+    </div>
+
+
+    <div class="row row-2">
+      <div class="col-md title-2-small">
+        <div class="title-2" onclick="location.href ='/calm'" >Nyugalom</div><a href="/calm">Tovább ></a>
+        <div class="text-2">A Pilis lábánál és Dunakanyar szívében garantált a kikapcsolódás.</div>
+      </div>
+      <div class="col-md-8">
+        <div data-aos="fade-right" data-aos-duration="1000" data-aos-offset="200">
+          <img class="img-fluid" src="img/teli-8.jpg">
+        </div>
+      </div>
+      <div class="col-md title-2-big">
+        <div class="title-2" onclick="location.href ='/calm'" >Nyugalom</div><a href="/calm">Tovább ></a>
+        <div class="text-2">A Pilis lábánál és Dunakanyar szívében garantált a kikapcsolódás.</div>
+      </div>
+    </div>
+
+    <div class="row row-2">
+      <div class="col-md">
+        <div class="title-2" onclick="location.href ='/terrace'" >Fedett grillterasz</div><a href="/terrace">Tovább ></a>
+        <div class="text-2">Még rossz időben is megannyi elfoglaltság közül választhattok a házon belül és kívül
+          egyaránt.</div>
+      </div>
+      <div class="col-md-8">
+        <div data-aos="fade-left" data-aos-duration="1000" data-aos-offset="200">
+          <img class="img-fluid" src="img/belso-1.jpg">
+        </div>
+      </div>
+    </div>
+
+    <div class="row row-2">
+      <div class="col-md title-2-small">
+        <div class="title-2" onclick="location.href = '/rooms'" >Kényelmes és tágas szobák</div><a href="/rooms">Tovább ></a>
+        <div class="text-2">Egyedileg berendezett, tágas terek találhatóak a házon belül, hogy garantált legyen a kikapcsolódás.</div>
+      </div>
+      <div class="col-md-8">
+        <div data-aos="fade-right" data-aos-duration="1000" data-aos-offset="200">
+          <img class="img-fluid" src="img/belso-2.jpg">
+        </div>
+      </div>
+      <div class="col-md title-2-big">
+        <div class="title-2" onclick="location.href = '/rooms'" >Kényelmes és tágas szobák</div><a href="/rooms">Tovább ></a>
+        <div class="text-2">Egyedileg berendezett, tágas terek találhatóak a házon belül, hogy garantált legyen a kikapcsolódás.</div>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="container-fluid container-2">
+    <div class="row row-2">
+      <div class="container">
+        <img src="svgs/villa-alaprajz.svg"></img>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid container-2">
+    <div class="row row-2">
+      <div class="title-main">Szolgáltatásaink</div>
+      <hr>
+    </div>
+  <div class="row">
+    <div class="d-flex flex-wrap justify-content-center service-list">
+      <div class="service-item">
+        <img class="service-icon" src="svgs/pool-vector.svg">
+        <div class="service-text">Medence</div>
+      </div>
+      <div class="service-item">
+        <img class="service-icon" src="svgs/sauna-vector.svg">
+        <div class="service-text">Szauna</div>
+      </div>
+      <div class="service-item">
+        <img class="service-icon" src="svgs/tv-vector.svg">
+        <div class="service-text">Tv</div>
+      </div>
+      <div class="service-item">
+        <img class="service-icon" src="svgs/airconditioning-vector.svg">
+        <div class="service-text">Légkondi</div>
+      </div>
+      <div class="service-item">
+        <img class="service-icon" src="svgs/wifi-vector.svg">
+        <div class="service-text">Wifi</div>
+      </div>
+      <div class="service-item">
+        <img class="service-icon" src="svgs/grill-vector.svg">
+        <div class="service-text">Grill</div>
+      </div>
+      <div class="service-item">
+        <img class="service-icon" src="svgs/parking-vector.svg">
+        <div class="service-text">Parkoló</div>
+      </div>
+      <div class="service-item">
+        <img class="service-icon" src="svgs/sunbath-vector.svg">
+        <div class="service-text">Napozóágy</div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include('footer.php'); ?>
